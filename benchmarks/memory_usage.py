0ed9acfcf07756d3801d1901c73aa8ea248d63a8
TobiPristupin <tobipristupin@gmail.com>
2021-04-28 19:16:31 -0500

    Created delayed collection benchmark

--- benchmarks/memory_usage.py
+++ benchmarks/memory_usage.py
@@ -12,13 +12,34 @@ lines = log_file.readlines()
 log_file.close()
 
 start_time, entries, end_time = int(lines[0]), lines[1:-1], int(lines[-1])
+print(end_time - start_time)
 entries = list(map(lambda entry: entry.replace("\n", "").split(), entries))
-bytes_allocated_mb = [int(entry[4]) / 1e6 for entry in entries]
-allocation_timestamps = [(int(entry[5]) - start_time) for entry in entries]
-print(bytes_allocated_mb)
+#following lists are in megabytes
+allocations, sweeps, deallocations = [[], []], [[], []], [[], []] #first array element has x values, second has y values
+for entry in entries:
+    megabytes = int(entry[4]) / 1e6
+    timestamp = int(entry[5]) - start_time
+    if entry[0] == "Allocated":
+        allocations[0].append(timestamp)
+        allocations[1].append(megabytes)
+    elif entry[0] == "Sweeped":
+        sweeps[0].append(timestamp)
+        sweeps[1].append(megabytes)
+    elif entry[0] == "Deallocated":
+        deallocations[0].append(timestamp)
+        deallocations[1].append(megabytes)
+
+plt.style.use('ggplot')
 
 fig, ax = plt.subplots()
-ax.plot(allocation_timestamps, bytes_allocated_mb, linestyle='--', marker='o', color='b')
+
+ax.plot(allocations[0] + sweeps[0] + deallocations[0], allocations[1] + sweeps[1] + deallocations[1], "--", color="grey")
+
+ax.scatter(allocations[0], allocations[1], label="Allocation", color="blue", s=75)
+ax.scatter(sweeps[0], sweeps[1], label="GC Deletion", color="green", s=75)
+ax.scatter(deallocations[0], deallocations[1], label="Deallocation (Program Terminates)", color="red", s=75)
+
+ax.legend()
 ax.set_xlabel("Time")
 ax.set_ylabel("Memory (Megabytes)")
 plt.show()
